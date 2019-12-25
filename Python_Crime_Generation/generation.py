cc6ce4a25f7cfb760da31225179e45a39e475210
Julian Rex <julian.rechsteiner@gmail.com>
2019-12-25 13:10:21 -0700

    location data generation done

--- Python_Crime_Generation/generation.py
+++ Python_Crime_Generation/generation.py
@@ -1,17 +1,30 @@
 import csv
 import os
+import json
+
+
+'''
+Gets absolute path from json file
+'''
+def import_path():
+    with open("pass.json") as json_file:
+        path = json.load(json_file)
+        path = path['path']
+        print(type(path))
+        return path
+
 
 '''
 Extracts data from FBI crime csv files in Extracted Data.
 :params int year: due to how the folders are structured, pass a year
 '''
-def extract_locations(year):
+def extract_locations(year, abs_path):
 
-    os.chdir(f"../Extracted Data/{year}")
-    current_directory = os.getcwd()
+    # os.chdir(f"{abs_path}/{year}")
+    # current_directory = os.getcwd()
 
-
-    with open(f"{current_directory}/{year}.csv") as csv_file:
+    print(f"{abs_path}/{year}/{year}.csv")
+    with open(f"{abs_path}/{year}/{year}.csv") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
         state_stats = {}
@@ -31,13 +44,12 @@ def extract_locations(year):
             city = row[1]
 
             crime_header = ("Population", "Violent", "Murder", "Rape", "Robbery", "Aggravated", "Property", "Burglary", "Larceny", "Motor")
-            rape = row[5] if row[4] == "" else row[4]
+            rape = row[5] if row[4] == "" and year == 2016 else row[4]
             #rape = check_rape(row[5], row[6])
             crimes = [row[2], row[3], rape, row[6], row[7], row[8], row[9], row[10], row[11], row[12]]
             city_stats = { city : crimes }
 
             if state != "" and current_state == "" and prev_state == "": # initial
-                print("first loop:")
                 current_state = state
                 prev_state = state
             elif state != "": # new state
@@ -58,12 +70,15 @@ def extract_locations(year):
             # print(f"Previous state: {prev_state}")
             # print(f"State: {state}")
             # print(f"City: {city}\n")
+
             # limit
             # if(line_count > 400):
             #     break
             line_count += 1
 
-        print_dict(state_stats)
+        print(f"Processed {line_count} lines.")
+        # print_dict(state_stats)
+        return state_stats
 
 
 '''
@@ -77,9 +92,17 @@ def print_dict(dict):
             print(f"\tCity: {city}")
 
 
+def print_all(list):
+    for item in list:
+        print_dict(item)
+
+
 def main():
-    year = 2016
-    extract_locations(year)
+    abs_path = import_path()
+    data = []
+    year2016 = extract_locations(2016, abs_path)
+    #year2017 = extract_locations(2017, abs_path)
+    print_dict(year2016)
 
 
 main()
