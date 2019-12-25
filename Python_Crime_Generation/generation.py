01d5fd05e6d7225c0a508098ee8b28bec0c793b9
Julian Rex <julian.rechsteiner@gmail.com>
2019-12-25 12:30:55 -0700

    Data extraction works great!

--- Python_Crime_Generation/generation.py
+++ Python_Crime_Generation/generation.py
@@ -1,11 +1,10 @@
 import csv
 import os
-import pprint
 
-
-current_directory = os.getcwd()
-# print(f"Original: {current_directory}")
-
+'''
+Extracts data from FBI crime csv files in Extracted Data.
+:params int year: due to how the folders are structured, pass a year
+'''
 def extract_locations(year):
 
     os.chdir(f"../Extracted Data/{year}")
@@ -15,9 +14,11 @@ def extract_locations(year):
     with open(f"{current_directory}/{year}.csv") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         line_count = 0
-        crime_by_state = {}
+        state_stats = {}
         current_state = ""
-        city_info_array = []
+        prev_state = ""
+        city_stats_array = []
+
         for row in csv_reader:
 
             # skip header lines
@@ -25,37 +26,60 @@ def extract_locations(year):
                 line_count += 1
                 continue
 
-            # print(f"Current Row: {row}")
-
-            state_stats = {}
             state = row[0]
+            city_stats = {}
             city = row[1]
+
             crime_header = ("Population", "Violent", "Murder", "Rape", "Robbery", "Aggravated", "Property", "Burglary", "Larceny", "Motor")
-            rape = check_rape(row[5], row[6])
+            rape = row[6] if row[5] == "" else row[5]
+            #rape = check_rape(row[5], row[6])
             crimes = [row[2], row[3], row[4], rape, row[7], row[8], row[9], row[10], row[11], row[12]]
+            city_stats = { city : crimes }
 
-            if(state != ""):
+            if state != "" and current_state == "" and prev_state == "": # initial
+                print("first loop:")
                 current_state = state
-                crime_by_state.update({ current_state : city_info_array})
-                city_info_array = []
+                prev_state = state
+            elif state != "": # new state
+                state_stats[prev_state] = city_stats_array
+                city_stats_array = []
+                prev_state = state
+                current_state = state
+                # break if reaches footer
+                if row[0] != "" and row[0][0].isdigit():
+                    break
+            else: # same state as prev
+                state = current_state
 
-            state_stats.update({ city : crimes})
-            city_info_array.append(state_stats)
-            # print(city_info_array)
+            city_stats_array.append(city_stats)
 
-
-            line_count += 1
+            # print(f"Loop {line_count-4}")
+            # print(f"Current state: {current_state}")
+            # print(f"Previous state: {prev_state}")
+            # print(f"State: {state}")
+            # print(f"City: {city}\n")
             # limit
+            # if(line_count > 400):
+            #     break
+            line_count += 1
 
-        pprint(crime_by_state)
-
-def check_rape(r1, r2):
-
-    if(r1 == ""):
-        return r2
-    else:
-        return r1
+        print_dict(state_stats)
 
 
-year = 2016
-extract_locations(year)
+'''
+Pretty prints the final dictionary with all the necessary data.
+:params dictionary dict: pass in a dictionary
+'''
+def print_dict(dict):
+    for state, states in dict.items():
+        print(f"State: {state}")
+        for city in states:
+            print(f"\tCity: {city}")
+
+
+def main():
+    year = 2016
+    extract_locations(year)
+
+
+main()
