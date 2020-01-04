07e759930b63c30178010410174a3ffc9e46be2e
Julian Rex <julian.rechsteiner@gmail.com>
2020-01-03 23:06:09 -0700

    created all crime gen files

---
+++ Python_Crime_Generation/Crime/crime_generation.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.insert(0, "/Users/myfatduck/OneDrive/Programs/Databases/Databases-US_Crime/Python_Crime_Generation")
+import util
+import json
+
+
+def generate_crime():
+    
+
+
+def main():
+    generate_crime()
+
+
+main()
