02dd39d1e5235c17f7ccaf91c639c87fa21ff8f5
Julian Rex <julian.rechsteiner@gmail.com>
2020-01-03 18:55:51 -0700

    Person Generation complete / Util updates on outputting SQL and JSON

--- Python_Crime_Generation/Person/person_generation.py
+++ Python_Crime_Generation/Person/person_generation.py
@@ -2,6 +2,11 @@ import sys
 sys.path.insert(0, "/Users/myfatduck/OneDrive/Programs/Databases/Databases-US_Crime/Python_Crime_Generation")
 import util
 import csv
+import random
+
+# number of people to generate
+TOTAL_PERSON_NUM = 1000
+
 
 
 def import_first_names():
@@ -11,7 +16,6 @@ def import_first_names():
     with open(file, "r") as person_file:
         for line in person_file:
             data = line.split("\t")
-            print(data)
             for index, token in enumerate(data):
                 if token.isalpha():
                     if index == 1:
@@ -33,22 +37,59 @@ def import_last_names():
     return last_names_array
 
 
-def output_names(first_names):
+def output_json(first_names, last_names, people):
     util.output_json(first_names, "first_names.json")
-
-def output_last_names(last_names):
     util.output_json(last_names, "last_names.json")
+    util.output_json(people, "people.json")
 
 
 
-def generate_person():
+def choose_race(races):
+    ranks = []
+    current = 0.00
+    for race, percentage in races.items():
+        temp = [race, current, (current + percentage)]
+        ranks.append(temp)
+        current = current + percentage + 0.01
+
+    num = float('{0:.2f}'.format(random.uniform(0, 113)))
+    for item in ranks:
+        if item[1] <= num and num <= item[2]:
+            return item[0] # race
+
+    return "white"
+
+
+# this will output to output_SQL in util.
+# format must be a list of dictionaries.
+# a dictionary will be a person with: gender, race, ssn, names,
+def generate_person(genders, races, first_names, last_names):
+    people = []
+    ssn = 1 # initial SSN
+    for i in range(TOTAL_PERSON_NUM):
+        gender = genders[random.randint(0,1)]
+        race = choose_race(races)
+        fname = random.choice(first_names['Male']) if gender == 'M' else random.choice(first_names['Female'])
+        lname = random.choice(last_names)
+        num = random.random()
+        victim = True if num < 0.6 else False
+        offender = not victim
+        person = {"SSN": str(ssn).zfill(9), "gender": gender, "race": race, "Firstname": fname, "Lastname": lname, "Victim": victim, "Offender": offender}
+        people.append(person)
+        ssn += 1
+
+    return people
+
+
+def generation_reqs():
     genders = ["M", "F"]
     races = {"American Indian": 0.5, "Alaskan Native": 0.4, "Asian": 4.8, "African American": 12.6, "Hispanic": 16.3, "Native Hawaiian": 0.1, "Other Pacific Islanders": 0.1, "White": 72.4, "Other":6.4}
     first_names = import_first_names()
-    output_names(first_names)
     last_names = import_last_names()
-    output_last_names(last_names)
+    people = generate_person(genders, races, first_names, last_names)
 
+    util.output_SQL("PERSON", people, "/mysql/insertPeople.sql")
+    output_json(first_names, last_names, people)
 
 
 
@@ -56,6 +97,6 @@ def generate_person():
 
 
 def main():
-    generate_person()
+    generation_reqs()
 
 main()
--- Python_Crime_Generation/util.py
+++ Python_Crime_Generation/util.py
@@ -17,6 +17,9 @@ def import_project_path():
 def process(lines):
     print(f"Processed {lines} lines." )
 
+def outputted(lines):
+    print(f"Outputted {lines} lines." )
+
 # gets type of $item
 def t(item):
     print(f"Type: {type(item)}")
@@ -49,13 +52,44 @@ def print_dict(dictionary):
 # function that creates SQL output
 # assumes a nested dictionary if data is type list
 def output_SQL(table_name, data, path_to_file):
+    # temp solutions for dicts with more than 1 value
+    if len(data[0].keys()) > 1:
+        output_multiple(table_name, data, path_to_file)
+        return
     if type(data) == list:
+        count = 0
         for index, item in enumerate(data, 1): # item is a dictionary
             for key in item:
                 file = f"{import_project_path()}/{path_to_file}"
                 with open(file, "a") as output_file:
                     line = f"INSERT INTO {table_name} VALUES({index}, '{key}', {item.get(key)});\n"
                     output_file.write(line)
+                    count += 1
+        outputted(count)
+
+# temp function for multiple dictionary values (such as person)
+def output_multiple(table_name, data, path_to_file):
+    values = []
+    for index, item in enumerate(data, 1): # item is a dictionary
+        temp = []
+        for key, value in item.items():
+            temp.append(value)
+        values.append(temp)
+
+    count = 0 # number of lines outputted
+    for item in values:
+        file = f"{import_project_path()}/{path_to_file}"
+        with open(file, "a") as output_file:
+            v = value_to_string(item)
+            line = f"INSERT INTO {table_name} VALUES({v});\n"
+            output_file.write(line)
+            count += 1
+    outputted(count)
+
+
+def value_to_string(item):
+    item = str(item)
+    return item[1:-1]
 
 def output_json(data, file_name, open_type = "w"):
     file = find_all(file_name)
